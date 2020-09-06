195422ebf6196c15d6b6433395857929ae002e53
Hugo Carvalho <webnoico1978@gmail.com>
2020-09-06 21:44:31 +0200

    Match Reports: added data. Player Charts: changed colors

--- src/data/matchReports.js
+++ src/data/matchReports.js
@@ -1,58 +1,154 @@
 const matchReport = [
   {
     id: 1,
-    date: "02-02-20",
+    date: "06-07-17",
     playerA: {
       name: "Hugo",
-      team: "Boston",
+      team: "San Francisco",
       won: false,
       score: 3,
     },
     playerB: {
       name: "Eddie",
-      team: "New York",
+      team: "Los Angeles",
       won: true,
       score: 4,
     },
   },
   {
     id: 2,
-    date: "21-02-20",
+    date: "09-07-17",
     playerA: {
       name: "Hugo",
-      team: "New York",
-      won: false,
-      score: 2,
+      team: "Boston",
+      won: true,
+      score: 4,
     },
     playerB: {
       name: "Eddie",
-      team: "Boston",
+      team: "New York",
+      won: false,
+      score: 3,
+    },
+  },
+  {
+    id: 3,
+    date: "18-07-20",
+    playerA: {
+      name: "Eddie",
+      team: "San Francisco",
+      won: false,
+      score: 3,
+    },
+    playerB: {
+      name: "Hugo",
+      team: "New York",
       won: true,
       score: 4,
     },
   },
   {
-    id: 3,
-    date: "21-02-20",
+    id: 4,
+    date: "24-02-18",
     playerA: {
+      name: "Hugo",
+      team: "Los Angeles",
+      won: true,
+      score: 4,
+    },
+    playerB: {
+      name: "Eddie",
+      team: "Boston",
+      won: false,
+      score: 1,
+    },
+  },
+  {
+    id: 5,
+    date: "25-02-18",
+    playerA: {
+      name: "Hugo",
+      team: "New York",
+      won: true,
+      score: 4,
+    },
+    playerB: {
+      name: "Eddie",
+      team: "San Francisco",
+      won: false,
+      score: 3,
+    },
+  },
+  {
+    id: 6,
+    date: "10-05-18",
+    playerA: {
+      name: "Hugo",
+      team: "New York",
+      won: false,
+      score: 3,
+    },
+    playerB: {
+      name: "Eddie",
+      team: "San Francisco",
+      won: true,
+      score: 4,
+    },
+  },
+  {
+    id: 7,
+    date: "10-01-19",
+    playerA: {
+      name: "Hugo",
+      team: "Boston",
+      won: true,
+      score: 4,
+    },
+    playerB: {
+      name: "Eddie",
+      team: "Los Angeles",
+      won: false,
+      score: 2,
+    },
+  },
+  {
+    id: 8,
+    date: "17-10-19",
+    playerA: {
+      name: "Hugo",
+      team: "San Francisco",
+      won: false,
+      score: 3,
+    },
+    playerB: {
+      name: "Eddie",
+      team: "Los Angeles",
+      won: true,
+      score: 4,
+    },
+  },
+  {
+    id: 9,
+    date: "23-12-19",
+    playerA: {
+      name: "Hugo",
+      team: "Boston",
+      won: true,
+      score: 4,
+    },
+    playerB: {
       name: "Eddie",
       team: "Los Angeles",
       won: false,
       score: 1,
     },
-    playerB: {
-      name: "Hugo",
-      team: "Boston",
-      won: true,
-      score: 4,
-    },
   },
   {
-    id: 2,
-    date: "02-02-20",
+    id: 10,
+    date: "30-08-20",
     playerA: {
       name: "Hugo",
-      team: "Los Angeles",
+      team: "Boston",
       won: true,
       score: 4,
     },
@@ -63,38 +159,6 @@ const matchReport = [
       score: 0,
     },
   },
-  // {
-  //   id: 1,
-  //   date: "21-02-20",
-  //   playerA: {
-  //     name: "Hugo",
-  //     team: "Boston",
-  //     won: true,
-  //     score: 4,
-  //   },
-  //   playerB: {
-  //     name: "Eddie",
-  //     team: "San Francisco",
-  //     won: false,
-  //     score: 2,
-  //   },
-  // },
-  // {
-  //   id: 1,
-  //   date: "21-02-20",
-  //   playerA: {
-  //     name: "Hugo",
-  //     team: "Los Angeles",
-  //     won: false,
-  //     score: 2,
-  //   },
-  //   playerB: {
-  //     name: "Eddie",
-  //     team: "Boston",
-  //     won: true,
-  //     score: 4,
-  //   },
-  // },
 ];
 
 export default matchReport;
