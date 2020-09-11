8805b401defd351fb7890749edd3d861f2960cd4
Hugo Carvalho <webnoico1978@gmail.com>
2020-09-11 21:13:31 +0200

    Added page counter

--- src/App.js
+++ src/App.js
@@ -14,12 +14,6 @@ function App() {
         <PlayersStats />
       </main>
       <a href="https://www.counters-free.net/">https://www.counters-free.net</a>{" "}
-      <script
-        type="text/javascript"
-        src="https://www.freevisitorcounters.com/auth.php?id=fb5edd82ad0ef336f64829a20662a951acbfbbbc"></script>
-      <script
-        type="text/javascript"
-        src="https://www.freevisitorcounters.com/en/home/counter/744240/t/2"></script>
     </div>
   );
 }
