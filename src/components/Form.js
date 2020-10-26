2aed200dd9b8d126ecdcaa0c4ed8ede4d87e9c35
Hugo Carvalho <webnoico1978@gmail.com>
2020-10-26 17:31:18 +0100

    PlayerForm: abstracted logic for reusable component. Refactored

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,4 +1,5 @@
 import React from "react";
+import PlayerForm from "../lostCities/PlayerForm";
 import "./Form.scss";
 
 const database = {
@@ -6,7 +7,7 @@ const database = {
   players: [],
 };
 
-//Component
+//ComrAnt
 //1 - Form
 //2 - Player Input
 
@@ -27,35 +28,33 @@ export function RenderLastResult() {
 
 export function Form() {
   const [matchesDatabase, setMatchesDatabase] = React.useState(database);
-  const [all, setAll] = React.useState();
-  const [arr, setArr] = React.useState({ games: [] });
-
-  Object.freeze(arr);
-  Object.freeze(arr.games);
+  const [total, setTotal] = React.useState(0);
 
   const handleSubmit = (e) => {
     e.preventDefault();
+
+    console.log("HANDL SUBMIT");
     // setArr((state) => {
     //   return { ...state, games: [...state.games, 21] };
     // });
 
-    const { calc, namePlayerOne, round1, round2, round3 } = {
-      namePlayerOne: e.target.elements["player01-name"].value,
-      round1: e.target.elements["player01-round1"].value,
-      round2: e.target.elements["player01-round2"].value,
-      round3: e.target.elements["player01-round3"].value,
+    const { calc, namePlayerA, round1, round2, round3 } = {
+      namePlayerA: e.target.elements["playerA-name"].value,
+      round1: e.target.elements["playerA-round1"].value,
+      round2: e.target.elements["playerA-round2"].value,
+      round3: e.target.elements["playerA-round3"].value,
       calc() {
         return +round1 + +round2 + +round3;
       },
     };
 
-    const playerTwo = {
-      name: e.target.elements["player02-name"].value,
-      round1: Number(e.target.elements["player02-round1"].value),
-      round2: Number(e.target.elements["player02-round2"].value),
-      round3: Number(e.target.elements["player02-round3"].value),
+    const playerB = {
+      name: e.target.elements["playerB-name"].value,
+      round1: Number(e.target.elements["playerB-round1"].value),
+      round2: Number(e.target.elements["playerB-round2"].value),
+      round3: Number(e.target.elements["playerB-round3"].value),
     };
-
+    console.log(playerB);
     setMatchesDatabase((state) => {
       return {
         ...state,
@@ -64,38 +63,43 @@ export function Form() {
           {
             id: 1,
             date: Date.now(),
-            playerOne: {
-              name: namePlayerOne,
+            playerA: {
+              name: namePlayerA,
               rounds: [+round1, +round2, +round3],
               total: calc(),
             },
-            playerTwo: {
-              name: playerTwo.name,
-              rounds: [playerTwo.round1, playerTwo.round2, playerTwo.round3],
-              total: playerTwo.round1 + playerTwo.round2 + playerTwo.round3,
+            playerB: {
+              name: playerB.name,
+              rounds: [playerB.round1, playerB.round2, playerB.round3],
+              total: playerB.round1 + playerB.round2 + playerB.round3,
             },
           },
         ],
       };
     });
 
-    document.querySelector("form").reset();
+    // document.querySelector("form").reset();
   };
 
   React.useEffect(() => {
-    // console.dir("MATCHES DATABASE EFFECT", matchesDatabase);
-    // console.log("games:", games);
     console.log("database:", matchesDatabase);
-    // console.log("arr", arr);
-  }, [matchesDatabase, all, arr]);
+  }, [matchesDatabase]);
+
+  React.useEffect(() => {
+    // console.log("TOTAL", total);
+  }, [total]);
 
   // const fecha = new Date(1603661548350);
-  const fecha = new Date(matchesDatabase.games[0] ? matchesDatabase.games[0].date : "");
-
+  const fecha = matchesDatabase.games[0] ? new Date(matchesDatabase.games[0].date) : null;
+  // console.log(fecha);
   return (
     <>
+      <section>Games History</section>
+      <p>{matchesDatabase.games.length} games were played</p>
+      <div>
+        date + similar to last fixture (design similar to my todo list) + delete + edit{" "}
+      </div>
       {/* RENDER LAST RESULT */}
-
       <section>
         <h2>Last Fixture</h2>
         {/* <time>
@@ -111,81 +115,59 @@ export function Form() {
         {/* <time>Date: {fecha ? fecha.toDateString() : ""}</time> */}
         <time>
           Date:{" "}
-          {fecha.toLocaleString("de", {
-            year: "numeric",
-            day: "numeric",
-            month: "numeric",
-          })}
+          {fecha
+            ? fecha.toLocaleString("de", {
+                year: "numeric",
+                day: "numeric",
+                month: "numeric",
+              })
+            : null}
         </time>
+
+        <div>
+          <h3>
+            Name:{" "}
+            {matchesDatabase.games[0]
+              ? matchesDatabase.games.reverse()[0].playerA.name // !!! Reverses in place !!!
+              : ""}
+            <span>
+              Rounds:
+              {matchesDatabase.games[0]
+                ? `${matchesDatabase.games[0].playerA.rounds[0]} - ${matchesDatabase.games[0].playerA.rounds[1]} - ${matchesDatabase.games[0].playerA.rounds[2]}`
+                : ""}{" "}
+            </span>
+            <span>
+              Total Score:{" "}
+              {matchesDatabase.games[0] ? matchesDatabase.games[0].playerA.total : ""}
+            </span>
+            <span>edit</span>
+          </h3>
+        </div>
+        <div>
+          <h3>
+            Name: {matchesDatabase.games[0] ? matchesDatabase.games[0].playerB.name : ""}
+            <span>
+              Rounds:
+              {matchesDatabase.games[0]
+                ? `${matchesDatabase.games[0].playerB.rounds[0]} - ${matchesDatabase.games[0].playerB.rounds[1]} - ${matchesDatabase.games[0].playerB.rounds[2]}`
+                : ""}{" "}
+            </span>
+            <span>
+              Total Score:{" "}
+              {matchesDatabase.games[0] ? matchesDatabase.games[0].playerB.total : ""}
+            </span>
+            <span>edit</span>
+          </h3>
+        </div>
+
         {/* <time>Date: {fecha}</time> */}
         <p>Add result:</p>
         <button>+</button>
       </section>
       {}
       <form className="Form" onSubmit={handleSubmit}>
-        {/* Do component of player form */}
-        <div className="player-input">
-          <h2>Player 1</h2>
-          <label htmlFor="player01-name">Player name</label>
-          <input
-            id="player01-name"
-            name="player01-name"
-            type="text"
-
-            // onChange={(e) => setInputText(e.target.value)}
-          />
-          {/* ROUNDS */}
-          <div className="wrapper__rounds">
-            <label htmlFor="player01-round1">Round 1</label>
-            <input id="player01-round1" name="player01-round1" type="number" />
-            <label htmlFor="player01-round1">Round 2</label>
-            <input id="player01-round2" name="player01-round2" type="number" />
-            <label htmlFor="player01-round1">Round 3</label>
-            <input id="player01-round3" name="player01-round3" type="number" />
-          </div>
-        </div>
-
-        <label htmlFor="player01-total">Total</label>
-        <input
-          id="player01-total"
-          name="player01-total"
-          readOnly
-          // onChange={(e) => setInputText(e.target.value)}
-        />
-
-        <div>
-          <button type="submit">Submit</button>
-        </div>
-
-        <div className="player-input">
-          <h2>Player 2</h2>
-          <label htmlFor="player02-name">Player name</label>
-          <input
-            id="player02-name"
-            name="player02-name"
-            type="text"
-
-            // onChange={(e) => setInputText(e.target.value)}
-          />
-          {/* ROUNDS */}
-          <div className="wrapper__rounds">
-            <label htmlFor="player02-round1">Round 1</label>
-            <input id="player02-round1" name="player02-round1" type="number" />
-            <label htmlFor="player02-round2">Round 2</label>
-            <input id="player02-round2" name="player02-round2" type="number" />
-            <label htmlFor="player02-round3">Round 3</label>
-            <input id="player02-round3" name="player02-round3" type="number" />
-          </div>
-        </div>
-
-        <label htmlFor="player02-total">Total</label>
-        <input
-          id="player02-total"
-          name="player02-total"
-          readOnly
-          // onChange={(e) => setInputText(e.target.value)}
-        />
-        {/* PLAAYER 2 */}
+        <PlayerForm player="playerA" matchesDatabase={matchesDatabase} />
+        <PlayerForm player="playerB" matchesDatabase={matchesDatabase} />
         <div>
           <button type="submit">Submit</button>
         </div>
---
+++ src/lostCities/PlayerForm.js
@@ -0,0 +1,49 @@
+import React from "react";
+
+function PlayerForm({ player, matchesDatabase }) {
+  return (
+    <div className="PlayerForm">
+      <div className="wrapper__name name-total">
+        <h2>
+          <label htmlFor={`${player}-name`}>Player name</label>
+        </h2>
+        <input
+          placeholder="Your name"
+          className="name"
+          id={`${player}-name`}
+          name={`${player}-name`}
+          type="text"
+        />
+      </div>
+
+      {/***  ROUNDS ***/}
+      <div className="wrapper__rounds">
+        <div className="wrapper__label-input">
+          <label htmlFor={`${player}-round1`}>Round 1</label>
+          <input id={`${player}-round1`} name={`${player}-round1`} type="number" />
+        </div>
+        <div className="wrapper__label-input">
+          <label htmlFor={`${player}-round2`}>Round 2</label>
+          <input id={`${player}-round2`} name={`${player}-round2`} type="number" />
+        </div>
+        <div className="wrapper__label-input">
+          <label htmlFor={`${player}-round3`}>Round 3</label>
+          <input id={`${player}-round3`} name={`${player}-round3`} type="number" />
+        </div>
+      </div>
+
+      {/***  TOTAL ***/}
+      <div className="wrapper__total name-total">
+        <label htmlFor="playerOne-total">Total</label>
+        <input
+          id="playerOne-total"
+          name="playerOne-total"
+          value={matchesDatabase.games[0] ? matchesDatabase.games[0][player].total : 0}
+          readOnly
+        />
+      </div>
+    </div>
+  );
+}
+
+export default PlayerForm;
